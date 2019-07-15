e070a9935d7ffac3279c528e504c2bc92e506209
Vojislav <vojislav.stupar@gmail.com>
2019-07-15 22:07:03 +0200

    zad3 getAll

--- src/components/AppMovies.vue
+++ src/components/AppMovies.vue
@@ -1,17 +1,44 @@
 <template>
     <div>
-        <h1>
-            PROBA
-        </h1>
+       <ul v-for="movie in movies" :key="movie.id">
+           <li>
+               {{ movie.title }}
+           </li>
+           <p> 
+               {{ movie.director}}
+           </p>
+           <p>
+               {{ movie.duration }}
+           </p>
+           <p>
+               {{ movie.releaseDate }}
+           </p>
+           <p>
+               {{ movie.genre }}
+           </p>
+       </ul>
     </div>
 </template>
 
 <script>
-export default {
+import MovieService from '../services/movies';
 
+export default {
+    data() {
+        return {
+            movies: []
+        }
+    },
+    beforeRouteEnter() {
+        movieService.getAll()
+        .then( response => {
+            this.movies= response.data;
+        })
+    }
 }
 </script>
 
 <style>
 
 </style>
+
--- src/services/movies.js
+++ src/services/movies.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-export default class MoovieService {
+export default class MovieService {
     constructor() {
         axios.base.baseURL = 'http://localhost:3000/api'
     }
 
-
+    getAll() {
+        return axios.get('/movies');
+    }
 }
 
-export const movieService= new MovieService()
